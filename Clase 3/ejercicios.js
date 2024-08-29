88b299ff0a3542aa6b63a904dbd120d550025868
jmagarinos <jmagarinos@itba.edu.ar>
2024-08-29 20:45:54 -0300

    Commit

---
+++ Clase 3/ejercicios.js
@@ -0,0 +1,36 @@
+// 1. Ejercicio 1: Función Declarada
+//    - Escribe una función que tome dos números como argumentos y retorne su suma. Llama a la función con diferentes pares de números e imprime el resultado.
+
+let var1 = require('prompt-sync')();
+let var2 = require('prompt-sync')();
+
+function nombreDeLaFuncion(a, b) {
+    return a + b
+}
+
+console.log(nombreDeLaFuncion(var1, var2))
+
+
+// 2. Ejercicio 2: Función Expresada
+//    - Crea una función expresada que reciba un array de números y retorne la suma de todos sus elementos.
+
+
+
+// 3. Ejercicio 3: Función Flecha
+//    - Convierte la función del ejercicio anterior a una función de flecha.
+
+
+
+// 4. Ejercicio 4: Estructura If-Else
+//    - Escribe una función que tome una edad como argumento y devuelva un mensaje que indique si la persona es un niño, adolescente, adulto o anciano.
+
+
+
+// 5. Ejercicio 5: Operador Ternario
+//    - Reescribe la función anterior usando un operador ternario para determinar si la persona es mayor de edad o no.
+
+
+
+// 6. Ejercicio 6: Operadores And y Or
+//    - Escribe una función que tome tres argumentos booleanos y retorne `true` si al menos uno de ellos es verdadero, o `false` si todos son falsos.
+
---
+++ Clase 3/prompt_sync.js
@@ -0,0 +1,4 @@
+let prompt = require('prompt-sync')();
+
+let nombre = prompt("Ingrese su nombre:")
+console.log(`Tu nombre es ${nombre}`)
